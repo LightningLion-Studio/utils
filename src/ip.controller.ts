e0b2c34994aa154e49fef7f2398001180253aa1b
Naily <zero@naily.cc>
2024-06-19 15:52:10 +0800

    refactor: baidu

--- src/ip.controller.ts
+++ src/ip.controller.ts
@@ -1,5 +1,6 @@
 import Elysia, { t } from 'elysia'
 import { ip } from 'elysia-ip'
+import type { AxiosResponse } from 'axios'
 import axios, { AxiosError } from 'axios'
 
 export const ipController = new Elysia({ prefix: '/ip', tags: ['IP'] })
@@ -27,40 +28,43 @@ export const ipController = new Elysia({ prefix: '/ip', tags: ['IP'] })
       }),
     },
   })
-  // eslint-disable-next-line ts/ban-ts-comment
-  // @ts-expect-error
   .get('/info', async (ctx) => {
+    let data: AxiosResponse
     try {
-      const data = await axios.get(`https://opendata.baidu.com/api.php?query=${ctx.query.ip || ctx.ip}&resource_id=6006`)
-      if (data.data.status !== '0') {
-        return {
-          code: 200,
-          message: '未知',
-          data: data.data,
-        }
-      }
-      else {
-        return {
-          code: 200,
-          message: data.data.data.location || '未知',
-          data: data.data,
-        }
-      }
+      data = await axios.get(`https://qifu-api.baidubce.com/ip/geo/v1/district?ip=${ctx.query.ip || ctx.ip}&timestamp=${new Date().getTime()}`)
     }
     catch (error) {
       if (error instanceof AxiosError) {
-        return {
-          code: 200,
-          message: '未知',
-          data: error.response ? error.response.data : error,
-        }
+        data = error.response!
       }
       else {
         return {
           code: 500,
-          message: '请求失败',
-          data: error,
+          message: '请求错误',
+          data: {},
         }
+      }
+    }
+
+    if (data.data.code === 'MissingParameter') {
+      return {
+        code: 400,
+        message: '参数缺失，获取不到当前IP信息',
+        data: data.data,
+      }
+    }
+    else if (data.data.code === 'Success') {
+      return {
+        code: 200,
+        message: 'OK',
+        data: data.data.data,
+      }
+    }
+    else {
+      return {
+        code: 400,
+        message: '参数错误',
+        data: data.data,
       }
     }
   }, {
@@ -70,8 +74,8 @@ export const ipController = new Elysia({ prefix: '/ip', tags: ['IP'] })
     },
     response: {
       200: t.Object({
-        code: t.Union([t.Literal(200), t.Literal(500)]),
-        message: t.String({ default: 'unknown', description: 'IP地址信息。有`未知`和`请求失败`的错误值' }),
+        code: t.Union([t.Literal(200, { description: '成功' }), t.Literal(500, { description: '请求错误' }), t.Literal(400, { description: '参数错误' })]),
+        message: t.String({ default: 'unknown', description: '状态信息。' }),
         data: t.Record(t.String(), t.Any()),
       }),
     },
