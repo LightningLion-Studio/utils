6dcf31906f097ae654c9199a5331e76933ee944b
Naily <zero@naily.cc>
2024-06-19 15:36:52 +0800

    feat: update

--- src/ip.controller.ts
+++ src/ip.controller.ts
@@ -1,4 +1,4 @@
-import Elysia from 'elysia'
+import Elysia, { t } from 'elysia'
 import { ip } from 'elysia-ip'
 import axios, { AxiosError } from 'axios'
 
@@ -14,13 +14,24 @@ export const ipController = new Elysia({ prefix: '/ip', tags: ['IP'] })
     }
   }, {
     detail: {
-      summary: '获取IP信息',
-      description: '获取IP信息。',
+      summary: '获取IP',
+      description: '直接获取IP。',
+    },
+    response: {
+      200: t.Object({
+        code: t.Literal(200),
+        message: t.String({ default: 'OK', description: '状态信息' }),
+        data: t.Object({
+          ip: t.String({ default: 'unknown', description: 'IP地址', format: 'ipv4' }),
+        }),
+      }),
     },
   })
+  // eslint-disable-next-line ts/ban-ts-comment
+  // @ts-expect-error
   .get('/info', async (ctx) => {
     try {
-      const data = await axios.get(`https://opendata.baidu.com/api.php?query=${ctx.ip}&resource_id=6006`)
+      const data = await axios.get(`https://opendata.baidu.com/api.php?query=${ctx.query.ip || ctx.ip}&resource_id=6006`)
       if (data.data.status !== '0') {
         return {
           code: 200,
@@ -39,7 +50,7 @@ export const ipController = new Elysia({ prefix: '/ip', tags: ['IP'] })
     catch (error) {
       if (error instanceof AxiosError) {
         return {
-          code: 500,
+          code: 200,
           message: '未知',
           data: error.response ? error.response.data : error,
         }
@@ -57,4 +68,14 @@ export const ipController = new Elysia({ prefix: '/ip', tags: ['IP'] })
       summary: '使用百度API获取IP信息',
       description: '获取IP信息(通过百度API)。',
     },
+    response: {
+      200: t.Object({
+        code: t.Union([t.Literal(200), t.Literal(500)]),
+        message: t.String({ default: 'unknown', description: 'IP地址信息。有`未知`和`请求失败`的错误值' }),
+        data: t.Record(t.String(), t.Any()),
+      }),
+    },
+    query: t.Object({
+      ip: t.Optional(t.String({ description: 'IP地址', default: '', format: 'ipv4' })),
+    }),
   })
